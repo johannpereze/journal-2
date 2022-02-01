864213e7f78048bd5916c0717d7220de3deaf85c
johannpereze <sebas.jsp@gmail.com>
2022-01-31 21:44:32 -0500

    Clase 231, hacemos los estilos del login

---
+++ src/components/journal/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Sidebar = () => {
+  return <aside className='journal__sidebar'>
+      <div className='journal__sidebar-navbar'></div>
+      <h3>
+          <span>Sebas</span>
+      </h3>
+  </aside>
+};
