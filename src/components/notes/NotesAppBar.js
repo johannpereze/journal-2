41a3b833eccaa1599181d5650a106d63fa9866e8
jperez-neostella <johann.perez@neostella.com>
2022-02-04 11:47:16 -0500

    Clase 235. Creamos la barra de las notas

---
+++ src/components/notes/NotesAppBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const NotesAppBar = () => {
+  return (
+    <div className="notes__appbar">
+      <span>28 de agosto de 2020</span>
+      <div>
+        <button className="btn">picture</button>
+        <button className="btn">save</button>
+      </div>
+    </div>
+  );
+};
