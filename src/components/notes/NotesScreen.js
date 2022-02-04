70b20bb8377053ddfa0b0b744ed611c7114ee5d4
jperez-neostella <johann.perez@neostella.com>
2022-02-04 12:23:37 -0500

    Clase 236. Terminamos el cascaron de la app

--- src/components/notes/NotesScreen.js
+++ src/components/notes/NotesScreen.js
@@ -5,6 +5,24 @@ export const NotesScreen = () => {
   return (
     <div className="notes__main-content">
       <NotesAppBar />
+      <div className="notes__content">
+        <input
+          type="text"
+          placeholder="Some awesome title"
+          className="notes__title-input"
+          autoComplete="off"
+        ></input>
+        <textarea
+          placeholder="Whats hapened today"
+          className="notes__textarea"
+        ></textarea>
+        <div className="notes__image">
+          <img
+            src="https://image.freepik.com/free-vector/river-tree-night-landscape_104785-154.jpg"
+            alt="note"
+          ></img>
+        </div>
+      </div>
     </div>
   );
 };
